e9851bf4b69f91859c45759fa83ba9097c4c8e88
MichalJaniec <michal.janiec95@gmail.com>
2024-02-11 20:55:59 +0100

    improve format number correct colors to imporve readability

--- src/components/atoms/product-description/ProductDescription.tsx
+++ src/components/atoms/product-description/ProductDescription.tsx
@@ -1,3 +1,5 @@
+import { formatMoney } from "@/utils";
+
 type ProductDescriptionProps = {
 	name: string;
 	category: string;
@@ -8,13 +10,13 @@ export const ProductDescription = ({ name, category, price }: ProductDescription
 	return (
 		<div className="mt-2 flex justify-between">
 			<div>
-				<h3 className="text-sm font-semibold text-gray-700">{name}</h3>
-				<p className="text-sm text-gray-500">
+				<h3 className="text-sm font-semibold text-gray-400">{name}</h3>
+				<p className="text-sm text-gray-300">
 					<span className="sr-only">Category</span> {category}
 				</p>
 			</div>
-			<p className="text-sm font-medium text-gray-900">
-				<span className="sr-only">Price</span> {price}$
+			<p className="text-sm font-medium text-gray-300">
+				<span className="sr-only">Price</span> {formatMoney(price)}
 			</p>
 		</div>
 	);
