dbc3620ac883662daae8d098da200a8ce885c07f
MichalJaniec <michal.janiec95@gmail.com>
2024-02-24 22:11:03 +0100

    updated types

--- src/components/types.ts
+++ src/components/types.ts
@@ -1,7 +1,10 @@
 export type ProductItem = {
+	id: string;
 	name: string;
 	category: string;
 	price: number;
+	description: string;
+	longDescription: string;
 	image: {
 		src: string;
 		alt: string;
