dd17b378ba9c9c29bb3bd4846e5e6543ba11ed40
MichalJaniec <michal.janiec95@gmail.com>
2024-02-24 22:13:30 +0100

    remove mock

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { MOCKED_PRODUCT_LIST } from "@/app/mocked-data";
 export default function Home() {
 	return (
 		<section className="mx-auto max-w-screen-2xl p-12">
-			<ProductsList products={MOCKED_PRODUCT_LIST} />
+			<h1 className="text-center text-6xl font-bold text-zinc-800">Welcome in ImpShop</h1>
 		</section>
 	);
 }
