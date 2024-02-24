0b09e02394f847aec947192bb67c8292f11e2ff2
MichalJaniec <michal.janiec95@gmail.com>
2024-02-24 22:50:21 +0100

    remove redudant import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import { ProductsList } from "@/components/organisms/products-list/ProductList";
 
-import { MOCKED_PRODUCT_LIST } from "@/app/mocked-data";
-
 export default function Home() {
 	return (
 		<section className="mx-auto max-w-screen-2xl p-12">
