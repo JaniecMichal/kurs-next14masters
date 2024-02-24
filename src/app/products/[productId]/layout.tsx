7a85279bcc6e968fa31f6d143524c6e6ae93d28c
MichalJaniec <michal.janiec95@gmail.com>
2024-02-24 22:13:57 +0100

    get product by id, add meta data

---
+++ src/app/products/[productId]/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { getProductById } from "@/api/products";
+
+export async function generateMetadata({
+	params,
+}: {
+	params: { productId: string };
+}): Promise<Metadata> {
+	const product = await getProductById(params.productId);
+
+	return {
+		title: product.name,
+		description: product.description,
+	};
+}
+
+export default function ProductLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return <section className="max-w-screen-2xl p-12 text-slate-800">{children}</section>;
+}
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,8 +1,13 @@
+import { getProductsList } from "@/api/products";
+import { ProductsList } from "@/components/organisms/products-list/ProductList";
 
-export default function Products() {
+export default async function Products() {
+	const productsToDisplay = await getProductsList();
+	
 	return (
 		<section className="mx-auto max-w-screen-2xl p-12">
 			<h1 className="mx-auto mb-4 text-center text-3xl text-slate-800">All products</h1>
+			<ProductsList products={productsToDisplay} />
 		</section>
 	);
 }
