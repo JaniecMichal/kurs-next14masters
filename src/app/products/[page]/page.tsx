004fd7c0db80e11b5414a06f8c1ac2c3fc40c7e3
MichalJaniec <michal.janiec95@gmail.com>
2024-02-27 22:40:53 +0100

    rename page

--- src/app/products/[page]/page.tsx
+++ src/app/products/[page]/page.tsx
@@ -2,7 +2,7 @@ import { getProductsList } from "@/api/products";
 
 import { ProductsList } from "@/components/organisms/products-list/ProductList";
 
-export default async function SingleProductPage({ params }: { params: { page: string } }) {
+export default async function ProductListPage({ params }: { params: { page: string } }) {
 	const productsToDisplay = await getProductsList(params.page);
 
 	return <ProductsList products={productsToDisplay} />;
