4fc4ca2d75f7c45459023eceae77c0cbd04126dd
MichalJaniec <michal.janiec95@gmail.com>
2024-02-27 22:37:58 +0100

    add simpliy pagination

--- src/components/molecules/pagination/Pagination.tsx
+++ src/components/molecules/pagination/Pagination.tsx
@@ -1,10 +1,19 @@
+"use client";
 import { ActiveLink } from "@/components/atoms/active-link";
+import type { Route } from "next";
+import { useParams } from "next/navigation";
 
 export const Pagination = () => {
+	const params = useParams();
+	const { page } = params;
+	const currentPage = page || "1";
+	const previousPage = Number(currentPage) > 1 ? Number(currentPage) - 1 : Number(currentPage);
+	const nextPage = Number(currentPage) + 1;
+
 	return (
 		<nav className="flex items-center justify-between gap-1" aria-label="pagination">
-			<ActiveLink href="/products" title="Previous Page" exact={true} />
-			<ActiveLink href="/products" title="Next Page" exact={true} />
+			<ActiveLink href={`/products/${previousPage}` as Route} title="Previous Page" exact={true} />
+			<ActiveLink href={`/products/${nextPage}` as Route} title="Next Page" exact={true} />
 		</nav>
 	);
 };
