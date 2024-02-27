b900c4bda1c8d351f104327e80c639f61702dda3
MichalJaniec <michal.janiec95@gmail.com>
2024-02-27 22:40:00 +0100

    removed unused import

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,5 @@
 import { getProductsList } from "@/api/products";
 import { ProductsList } from "@/components/organisms/products-list/ProductList";
-import Link from "next/link";
 
 export default async function Products() {
 	const productsToDisplay = await getProductsList();
