ba79b125a33e6d8f4578a53c3ef27f594308561e
MichalJaniec <michal.janiec95@gmail.com>
2024-03-04 21:16:15 +0100

    improve pagination, add static params

--- src/app/products/[page]/page.tsx
+++ src/app/products/[page]/page.tsx
@@ -2,6 +2,12 @@ import { getProductsList } from "@/api/products";
 
 import { ProductsList } from "@/components/organisms/products-list/ProductList";
 
+export async function generateStaticParams() {
+	const pagesParamToStaticGeneration = Array.from({ length: 10 }, (_, i) => i + 1);
+
+	return pagesParamToStaticGeneration;
+}
+
 export default async function ProductListPage({ params }: { params: { page: string } }) {
 	const productsToDisplay = await getProductsList(params.page);
 
--- src/components/molecules/pagination/Pagination.tsx
+++ src/components/molecules/pagination/Pagination.tsx
@@ -6,14 +6,24 @@ import { useParams } from "next/navigation";
 export const Pagination = () => {
 	const params = useParams();
 	const { page } = params;
-	const currentPage = page || "1";
+	const currentPage = (Array.isArray(page) && page[0]) || page || "1";
 	const previousPage = Number(currentPage) > 1 ? Number(currentPage) - 1 : Number(currentPage);
 	const nextPage = Number(currentPage) + 1;
 
 	return (
-		<nav className="flex items-center justify-between gap-1" aria-label="pagination">
-			<ActiveLink href={`/products/${previousPage}` as Route} title="Previous Page" exact={true} />
-			<ActiveLink href={`/products/${nextPage}` as Route} title="Next Page" exact={true} />
+		<nav className="flex items-center justify-between gap-1">
+			<ActiveLink
+				href={`/products/${previousPage}` as Route}
+				title="Previous Page"
+				exact={true}
+				aria-label="pagination"
+			/>
+			<ActiveLink
+				href={`/products/${nextPage}` as Route}
+				title="Next Page"
+				exact={true}
+				aria-label="pagination"
+			/>
 		</nav>
 	);
 };
