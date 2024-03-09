b27d3621e6561000d62f674cacdba3a3759cfd69
MichalJaniec <michal.janiec95@gmail.com>
2024-03-09 22:06:06 +0100

    move component to atoms use where needed

---
+++ src/components/atoms/loader/Loader.tsx
@@ -0,0 +1,9 @@
+export const Loader = () => {
+	return (
+		<div className="mx-auto text-center" aria-busy="true">
+			<p>
+				<svg className="... mr-3 h-5 w-5 animate-spin" viewBox="0 0 24 24"></svg>Loading...
+			</p>
+		</div>
+	);
+};
